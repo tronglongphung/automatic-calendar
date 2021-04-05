3ea6f3eae3bec8ca49b611a08ebd42762871e000
tronglongphung <tronglongphung@gmail.com>
2021-04-05 19:18:15 +0930

    commit

---
+++ Assets/script.js
@@ -0,0 +1,6 @@
+var container = document.querySelector('.container')
+
+
+var now = dayjs();
+$("#currentDay").text(`Today is ${now}`);
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # automatic-calendar
+
+Added current time
+Image at (./Assets/current-time-image.png)
